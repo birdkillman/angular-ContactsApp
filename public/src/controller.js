bc483ea5e6cbf5224118373e441309f4d25b9091
MoHo Khaleqi <habibsandyfan@gmail.com>
2017-07-25 07:45:52 +0100

    list of contacts, sort and filter them

--- public/src/controller.js
+++ public/src/controller.js
@@ -1,13 +1,17 @@
 angular.module('ContactsApp')
-	.controller('ListController', function($scope, Contact){
+	.controller('ListController', function($scope, Contact, $location){
 		$scope.contacts = Contact.query();
 		$scope.fields = ['firstName', 'lastName'];
 
-		$scope.sort = function ($field){
-			$scope.sort.field = $field;
+		$scope.sort = function (field){
+			$scope.sort.field = field;
 			$scope.sort.order = !$scope.sort.order;
 		}
 
 		$scope.sort.field = 'firstName';
-		$scope.sort.order = false;
+		$scope.sort.order = false; // Desc
+
+		$scope.show = function (id){
+			$location.url('/contact/' + id);
+		}
 	});
